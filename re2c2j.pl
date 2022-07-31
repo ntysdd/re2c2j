cfe5018fe91d7ce696b56a4818fcbf4f6ccb7015
ntysdd <ntysdd@gmail.com>
2022-07-31 18:11:59 +0800

    now more or less parse re2c output in simple cases

--- re2c2j.pl
+++ re2c2j.pl
@@ -86,18 +86,63 @@ use constant DEFINE_YYLABEL               => 'DEFINE_YYLABEL';
 use constant ASSIGN_YYCH_YYCURSOR         => 'ASSIGN_YYCH_YYCURSOR';
 use constant CMP_YYCH_LESS_EQ_CONSTANT    => 'CMP_YYCH_LESS_EQ_CONSTANT';
 use constant CMP_YYCH_EQ_CONSTANT_GOTO    => 'CMP_YYCH_EQ_CONSTANT_GOTO';
+use constant CMP_YYCH_NE_CONSTANT_GOTO    => 'CMP_YYCH_NE_CONSTANT_GOTO';
 use constant CMP_YYLIMIT_LESS_EQ_YYCURSOR => 'CMP_YYLIMIT_LESS_EQ_YYCURSOR';
 use constant CMP_YYFILL_0_GOTO            => 'CMP_YYFILL_0_GOTO';
 use constant GOTO_YYLABEL                 => 'GOTO_YYLABEL';
 use constant INC_YYCURSOR                 => 'INC_YYCURSOR';
+use constant ASSIGN_YYMARKER_INC_YYCURSOR => 'ASSIGN_YYMARKER_INC_YYCURSOR';
+use constant ASSIGN_YYMARKER_YYCURSOR     => 'ASSIGN_YYMARKER_YYCURSOR';
+use constant ASSIGN_YYCURSOR_YYMARKER     => 'ASSIGN_YYCURSOR_YYMARKER';
 
 my $C_SPACE = qr/( |\t|\v|\f)+/;
 
+my $is_in_user_code = 0;
+
+package UserCode;
+
+use overload '""' => '_stringify';
+
+sub new {
+    my ( $name, $usercode ) = @_;
+    my $self = { _usercode => $usercode };
+    bless $self, $name;
+    $self;
+}
+
+sub usercode {
+    my ($self) = @_;
+    $self->{_usercode};
+}
+
+sub _stringify {
+    my ($self) = @_;
+    my $usercode = $self->usercode;
+    "UserCode($usercode)";
+}
+
+package main;
+
 sub get_line_type($) {
     local $_ = $_[0];
+    if (m{\A#line [0-9]+ (.*)$}) {
+        if ( $1 eq q{"<stdout>"} ) {
+            $is_in_user_code = 0;
+        }
+        else {
+            $is_in_user_code = 1;
+        }
+        return LINE_COMMENT;
+    }
+
+    if ($is_in_user_code) {
+        return UserCode->new($_);
+    }
+
     if (m{\A$C_SPACE*\z}) {
         return EMPTY_LINE;
     }
+
     if (m{\A$C_SPACE*\Q/*\E(?:\s|\S)*\Q*/\E\z}) {
         my $comment_end_pos = index $_, '*/';
         if ( $comment_end_pos != length($_) - 2 ) {
@@ -105,27 +150,43 @@ sub get_line_type($) {
         }
         return COMMENT;
     }
-    if (m{\A#line }) {
-        return LINE_COMMENT;
-    }
+
     if (m<\A$C_SPACE*\Q{\E$C_SPACE*\z>) {
         return LEFT_BRACE;
     }
+
     if (m<\A$C_SPACE*\Q}\E$C_SPACE*\z>) {
         return RIGHT_BRACE;
     }
+
     if (m{\A$C_SPACE*YYCTYPE yych;$C_SPACE*\z}) {
         return DEFINE_YYCH;
     }
+
     if (m{\Ayy(?:eof|[0-9]+_?):\z}) {
         return DEFINE_YYLABEL;
     }
+
     if (m{\A$C_SPACE*\Qyych = *YYCURSOR;\E$C_SPACE*\z}) {
         return ASSIGN_YYCH_YYCURSOR;
     }
+
+    if (m{\A$C_SPACE*\QYYMARKER = YYCURSOR;\E$C_SPACE*\z}) {
+        return ASSIGN_YYMARKER_YYCURSOR;
+    }
+
+    if (m{\A$C_SPACE*\QYYMARKER = ++YYCURSOR;\E$C_SPACE*\z}) {
+        return ASSIGN_YYMARKER_INC_YYCURSOR;
+    }
+
+    if (m{\A$C_SPACE*\QYYCURSOR = YYMARKER;\E$C_SPACE*\z}) {
+        return ASSIGN_YYCURSOR_YYMARKER;
+    }
+
     if (m[\A$C_SPACE*\Qif (yych <= 0x\E[0-f]{2}\Q) {\E\z]) {
         return CMP_YYCH_LESS_EQ_CONSTANT;
     }
+
     if (
         m[\A$C_SPACE*
 	\Qif (yych == \E
@@ -136,9 +197,22 @@ sub get_line_type($) {
     {
         return CMP_YYCH_EQ_CONSTANT_GOTO;
     }
+
+    if (
+        m[\A$C_SPACE*
+	\Qif (yych != \E
+	(?:0x[a-f]{2}|'[a-z]')
+	\Q) goto \E
+	(?:yyeof|yy[0-9]+_?);\z]x
+      )
+    {
+        return CMP_YYCH_NE_CONSTANT_GOTO;
+    }
+
     if (m[\A$C_SPACE*\Qif (YYLIMIT <= YYCURSOR) {\E\z]) {
         return CMP_YYLIMIT_LESS_EQ_YYCURSOR;
     }
+
     if (
         m[\A$C_SPACE*
 	\Qif (YYFILL\E $C_SPACE*
@@ -148,9 +222,11 @@ sub get_line_type($) {
     {
         return CMP_YYFILL_0_GOTO;
     }
+
     if (m{\A$C_SPACE*goto (?:yyeof|yy[0-9]+_?);\z}) {
         return GOTO_YYLABEL;
     }
+
     if (m{\A$C_SPACE*\Q++YYCURSOR;\E\z}) {
         return INC_YYCURSOR;
     }
